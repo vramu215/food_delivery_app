62cb24c8e39618678f2b5d857db7735cf0a4cc2c
Ramu <ramudhavanapelli121@gmail.com>
2025-04-01 12:39:40 +0530

    Completed backend functionality

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,7 @@
-const { default: mongoose } = require('mongoose')
-
-constmongoose=require('mongoose')
-const UserSchema=mongoose.Schema({
+const mongoose=require('mongoose')
+const UserSchema = new mongoose.Schema({
     username:{type:String,require:true},
     email:{type:String,require:true},
     password:{type:String,require:true}
 })
-model.exports=mongoose.model('User',UserSchema)
+module.exports=mongoose.model('User',UserSchema)
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express=require('express')
 const mongoose=require('mongoose')
 require('dotenv').config()
-const User=require('.models/User.js')
+const User=require('./models/User')
+const bcrypt=require('bcryptjs')
 const PORT=3000
 
 const app=express()
+app.use(express.json())
 mongoose.connect(process.env.MONGO_URL).then(
     ()=>console.log("DB connected successfully......")
 ).catch(
@@ -20,8 +22,13 @@ app.get('/register',async(req,res)=>{
     }
 })
 app.post('/register',async(req,res)=>{
-    const {User,emial,password}=req.body
+    const {user,email,password}=req.body
     try{
+        const hashpassword=await bcrypt.hash(password,10)
+        const newUser=new User({user,email,password:hashpassword})
+        await newUser.save()
+        console.log("New user is registered successfully......")
+        res.json({message:'User created.......'})
 
     }
     catch(err){
@@ -30,6 +37,24 @@ app.post('/register',async(req,res)=>{
     }
 
 })
+//API loginpage
+app.post('/login',async(req,res)=>{
+    const{email,password}=req.body
+    try{
+        const user=await User.findOne({email});
+        if (!user || !(await bcrypt.compare(password,user.password)))
+        {
+            return res.status(400).json({message:"Invalid Credentials"});
+            
+        }
+        res.json({message:"Login Successfully",username:user.username});
+
+    }
+    catch(err){
+        console.log(err)
+    }
+})
+//server running and testing
 app.listen (PORT,(err)=>{
     if(err){
         console.log(err)
